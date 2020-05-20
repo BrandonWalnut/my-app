4c3fbdae6b8946c594abcbd111b99eeff7195cf6
BrandonWalnut <brandonwgamber@protonmail.com>
2020-05-19 21:12:54 -0500

    Added basic routing functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-     <Homepage></Homepage>
+      <Switch>
+        <Route exact path="/" component={Homepage}></Route>
+      </Switch>
     </div>
   );
 }
