47fc8745053d77164a923b934327026bc42fd4cb
BrandonWalnut <brandonwgamber@protonmail.com>
2020-05-21 08:04:36 -0500

    Constructed template for shop page with data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
 
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Homepage}></Route>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/colleciton-preview/collection-preview.component';
+
+class ShopPage extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+    render(){
+        const {collections} = this.state;
+        return(
+            <div className="shop-page"> 
+                SHOP PAGE
+                {collections.map(({id, ...otherCollectionProps}) => <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>)}
+            </div>
+        )
+    }
+}
+
+export default ShopPage
